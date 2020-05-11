1c228143226f04443acfbc27eb56c5cceac94e7b
zeeshanmir <zeeshanmir200@gmail.com>
2020-05-11 23:56:14 +0530

    Initial chnagdsfes

--- web/app.js
+++ web/app.js
@@ -1,21 +1,8 @@
 
 function alertMessage(text) {
 
-   
-const video = document.querySelector('video');
-const constraints = {
-    video: true
-  };
-navigator.mediaDevices.getUserMedia(constraints).
-  then((stream) => {video.srcObject = stream});
+  console.log('hello');
 
-  QCodeDecoder()
-  .decodeFromCamera(video, function(er,res){
-    console.log(res);
-    document.getElementById("result").innerHTML = res;
-  });
-
- 
 }
 
 
